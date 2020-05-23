09a3b718a54d3782871d25e12a9d32c635b3afbb
Will Weiss <will.weiss1230@gmail.com>
2020-05-23 14:15:49 -0400

    knex

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'prod') {
 
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 
-const connection = process.env.DB_URI || {
+const connection = process.env.DATABASE_URL || {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
